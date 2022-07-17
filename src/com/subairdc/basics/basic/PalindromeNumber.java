b9eabfca95e1807fa86de96c5a10d4dd600753d7
subairdc <subairdc@gmail.com>
2022-07-17 17:56:15 +0530

    Palindrome Number

---
+++ src/com/subairdc/basics/basic/PalindromeNumber.java
@@ -0,0 +1,42 @@
+package com.subairdc.basics.basic;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		
+		//Check the given number is palindrome or not
+		
+		int num;
+				
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		num = sc.nextInt();
+		
+		palindrome(num);
+
+	}
+
+	private static void palindrome(int num) {
+		
+		int temp,r,sum=0;
+		
+		temp=num;
+		
+		while(temp>1) {
+			r = temp%10;
+			sum = (sum*10) + r;
+			temp /= 10;
+		}
+		
+		if(sum == num) {
+			System.out.println("The given number "+ num +" is Palindrome");
+		}else
+			System.out.println("The given number "+ num +" is not a Palindrome");
+
+		
+	}
+
+}
